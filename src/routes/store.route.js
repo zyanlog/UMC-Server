d037a212e36be2977d3b384767cd2b6de1c0de68
Jieun Kim <imfunctionx@gamil.com>
2023-11-26 18:30:23 +0900

    docs: api 명세서 추가

--- src/routes/store.route.js
+++ src/routes/store.route.js
@@ -5,4 +5,14 @@ import { reviewPreview, missionPreview } from "../controllers/store.controller.j
 export const storeRouter = express.Router({mergeParams: true});
 
 storeRouter.post('/reviews', asyncHandler(reviewPreview));
-storeRouter.post('/missions', asyncHandler(missionPreview));
+storeRouter.get('/missions', asyncHandler(missionPreview));
+
+/**
+ * @swagger
+ * paths:
+ *  /{storeId}/missions:
+ *      get:
+ *          tags:
+ *              - Store
+ *          summary: 특정 가게의 미션 목록
+ */
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,5 +6,45 @@ export const userRouter = express.Router({mergeParams: true});
 
 userRouter.post('/signin', asyncHandler(userSignin));
 userRouter.post('/mission', asyncHandler(userMissionAdd));
-userRouter.post('/reviews', asyncHandler(reviewPreview));
+userRouter.get('/reviews', asyncHandler(reviewPreview));
 userRouter.get('/mission', asyncHandler(missionPreview));
+
+/**
+ * @swagger
+ * paths:
+ *  /{userId}/reviews:
+ *      get:
+ *          tags:
+ *              - User
+ *          summary: 내가 작성한 리뷰 목록
+ *          parameters:
+ *          - name: userId
+ *            in: path
+ *            schema:
+ *              type: integer
+ *            required: true
+ *          - name: reviewId
+ *            in: query
+ *            required: false
+ *            schema:
+ *            properties:
+ *              reviewId:
+ *                  type: integer
+ *          - name: paging
+ *            in: query
+ *            required: true
+ *            schema:
+ *              properties:
+ *                  size:
+ *                      type: integer 
+ */
+
+/**
+ * @swagger
+ * paths:
+ *  /{userId}/missions:
+ *      get:
+ *          tags:
+ *              - User
+ *          summary: 내가 진행중인 미션 목록
+ */
